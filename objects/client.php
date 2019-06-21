9d9197f7bf5a0015670d9919257405759858921e
William Duarte <william.2025@hotmail.com>
2019-06-21 16:01:50 -0400

    add delete and update client

--- objects/client.php
+++ objects/client.php
@@ -32,6 +32,30 @@ function read(){
  
     return $stmt;
 }
+// delete the client
+function delete(){
+ 
+    // delete query
+    $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+ 
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->id=htmlspecialchars(strip_tags($this->id));
+ 
+    // bind id of record to delete
+    $stmt->bindParam(1, $this->id);
+ 
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+     
+}
+
 // create client
 function create(){
  
